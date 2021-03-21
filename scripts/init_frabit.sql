d9f1496e4e3acd9edcc2b5a7e0fbe2e4a2634361
blylei <blylei@163.com>
2021-03-22 01:10:28 +0800

    init

--- scripts/init_frabit.sql
+++ scripts/init_frabit.sql
@@ -2,7 +2,7 @@
 CREATE USER 'frabit'@'localhost' IDENTIFIED BY 'Frabit@123';
 GRANT INSERT, SELECT, UPDATE, DELETE ON frabit.* TO 'frabit'@'localhost';
 
--- create frabit database used store backup policy and relvent info
+-- create frabit database used store backup policy and relevent info
 CREATE DATABASE frabit /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
 -- create tables within frabit
